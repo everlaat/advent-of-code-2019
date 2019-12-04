b9b414aba38547e84803baef31f738354d676518
Elvin Verlaat <everlaat@gmail.com>
2019-12-04 12:43:59 +1100

    Day03 part1 tests

--- tests/Suite.elm
+++ tests/Suite.elm
@@ -2,6 +2,7 @@ module Suite exposing (suite)
 
 import Solvers.Day01 as Day01
 import Solvers.Day02 as Day02
+import Solvers.Day03 as Day03
 import Test exposing (Test, describe)
 
 
@@ -10,4 +11,5 @@ suite =
     describe "Advent Of Code 2019"
         [ describe "Day 01" Day01.tests
         , describe "Day 02" Day02.tests
+        , describe "Day 03" Day03.tests
         ]
